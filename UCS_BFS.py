6864541c9307dfa79613d38d181f774453f3ca7b
Hồ Minh Quang <82497662+hydroq04@users.noreply.github.com>
2024-11-03 08:28:24 +0700

    Update UCS_BFS.py
    Update bad move detection

--- UCS_BFS.py
+++ UCS_BFS.py
@@ -1,7 +1,6 @@
 from maze import *
 import time
 import tracemalloc
-import numpy as np
 
 def BFS(game: SearchSpace) -> str:
     start_time = time.time()
@@ -9,33 +8,27 @@ def BFS(game: SearchSpace) -> str:
     open_set_capacity = len(game.open_set)
     goal = None
     nodes_created = len(game.open_set)
+
     while open_set_capacity > 0:
         new_nodes = game.nodeExpansion(game.open_set[0])
         nodes_created += len(new_nodes)
-        
-        # Remove redundant moves
-        # new_nodes = remove_redundant_moves(new_nodes)
 
         game.open_set += new_nodes
-
         open_set_capacity = len(game.open_set)
+
         if game.goalReached(game.closed_set[-1]):
             goal = game.closed_set[-1]
             break
-    if goal is None:
-        duration = (time.time() - start_time) * 1000
-        memory_peak = tracemalloc.get_traced_memory()[1]
-        tracemalloc.stop()
-        peak_in_MB = memory_peak / (1024 ** 2)
-        return f'''Steps: {0}, Weight: {0}, Node: {nodes_created}, Time (ms): {duration:.2f}, Memory (MB): {peak_in_MB:.2f}
-        {None}''', None            
+
     duration = (time.time() - start_time) * 1000
     memory_peak = tracemalloc.get_traced_memory()[1]
     tracemalloc.stop()
     peak_in_MB = memory_peak / (1024 ** 2)
-    road = game.path_construction(goal)
+    if goal is None:
+        return f'''Node: {nodes_created}, Time (ms): {duration:.2f}, Memory (MB): {peak_in_MB:.2f}
+No solution!'''
     return f'''Steps: {goal.steps}, Weight: {goal.weight}, Node: {nodes_created}, Time (ms): {duration:.2f}, Memory (MB): {peak_in_MB:.2f}
-{road}''', road
+{game.path_construction()}'''
 
 def UCS(game: SearchSpace) -> str:
     start_time = time.time()
@@ -44,41 +37,11 @@ def UCS(game: SearchSpace) -> str:
     goal = None
     nodes_created = len(game.open_set)
 
-    def agent_stone_distance(node: Node):
-        stone_pos = np.array(game.stones_state_list[node.stones_list_id])
-        agent_stone_diff = stone_pos - np.array(node.agent_pos)
-        Manhattan_dist = np.sum(np.abs(agent_stone_diff), axis = -1)
-        return Manhattan_dist
-
-    def remove_redundant_moves(node_set: list[Node]) -> list[Node]:
-        agent_stone_dist_curr = agent_stone_distance(game.closed_set[-1])
-
-        # Check if agent is moving further from all stones
-        # Remove moves that are not stone pushing
-        for node in node_set:
-            neighbor_cells = game.get_neighbors(node.agent_pos)
-            
-            surrounding_isEmpty = True
-            for neighbor in neighbor_cells:
-                for surrounding in game.surroundingCheck(neighbor):
-                    surrounding_isEmpty = surrounding_isEmpty and surrounding
-
-            agent_stone_dist_new = agent_stone_distance(node)
-            if surrounding_isEmpty and np.all(agent_stone_dist_curr + 1 < agent_stone_dist_new):
-                node_set.remove(node)
-
-        return node_set
-
     while open_set_capacity > 0:
         new_nodes = game.nodeExpansion(game.open_set[0])
         nodes_created += len(new_nodes)
-        
-        # Remove redundant moves
-        new_nodes = remove_redundant_moves(new_nodes)
 
-        game.open_set += new_nodes
-        game.open_set = sorted(game.open_set, key = lambda node: node.steps + node.weight)
-
+        game.open_set = sorted(game.open_set + new_nodes, key = lambda node: node.steps + node.weight)
         open_set_capacity = len(game.open_set)
 
         if game.goalReached(game.closed_set[-1]):
@@ -89,6 +52,8 @@ def UCS(game: SearchSpace) -> str:
     memory_peak = tracemalloc.get_traced_memory()[1]
     tracemalloc.stop()
     peak_in_MB = memory_peak / (1024 ** 2)
-    road = game.path_construction(goal)
+    if goal is None:
+        return f'''Node: {nodes_created}, Time (ms): {duration:.2f}, Memory (MB): {peak_in_MB:.2f}
+No solution!'''
     return f'''Steps: {goal.steps}, Weight: {goal.weight}, Node: {nodes_created}, Time (ms): {duration:.2f}, Memory (MB): {peak_in_MB:.2f}
-{road}''', road
+{game.path_construction()}'''
